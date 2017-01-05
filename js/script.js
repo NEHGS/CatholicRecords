054976e4d149258bf3ee7b56797de66020f52180
Anand Sharma <anand.sharma@datamatics.com>
2017-01-05 14:32:56 +0530

    Removed show More button on image articles

--- js/script.js
+++ js/script.js
@@ -284,35 +284,35 @@ $(document).ready(function () {
     });
 
 
-    // Toggle text on click more button display.
-    var showChar = 140;  // How many characters are shown by default
-    var ellipsestext = "...";
-    var moretext = "Show more";
-    var lesstext = "Show less";
-
-
-    $('.floated-box .image-caption-content').each(function () {
-      var content = $(this).html();
-      if (content.length > showChar) {
-        var c = content.substr(0, showChar);
-        var h = content.substr(showChar, content.length - showChar);
-        var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<button class="morelink btn">' + moretext + '</button></span>';
-        $(this).html(html);
-      }
-    });
-
-    $(".morelink").click(function () {
-      if ($(this).hasClass("less")) {
-        $(this).removeClass("less");
-        $(this).html(moretext);
-      } else {
-        $(this).addClass("less");
-        $(this).html(lesstext);
-      }
-      $(this).parent().prev().toggle('slow');
-      $(this).prev().toggle('slow');
-      return false;
-    });
+//    // Toggle text on click more button display.
+//    var showChar = 140;  // How many characters are shown by default
+//    var ellipsestext = "...";
+//    var moretext = "Show more";
+//    var lesstext = "Show less";
+//
+//
+//    $('.floated-box .image-caption-content').each(function () {
+//      var content = $(this).html();
+//      if (content.length > showChar) {
+//        var c = content.substr(0, showChar);
+//        var h = content.substr(showChar, content.length - showChar);
+//        var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<button class="morelink btn">' + moretext + '</button></span>';
+//        $(this).html(html);
+//      }
+//    });
+//
+//    $(".morelink").click(function () {
+//      if ($(this).hasClass("less")) {
+//        $(this).removeClass("less");
+//        $(this).html(moretext);
+//      } else {
+//        $(this).addClass("less");
+//        $(this).html(lesstext);
+//      }
+//      $(this).parent().prev().toggle('slow');
+//      $(this).prev().toggle('slow');
+//      return false;
+//    });
     
     
     // Js for timeline slideshow caption.
