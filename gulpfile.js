863104193ea0820d99f3078d091159c990b5a579
Andy Hanson-Dvoracek <andrew.hanson.dvoracek@nehgs.org>
2017-01-09 16:46:53 -0500

    Add useref and uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
-var concatCSS = require('gulp-concat-css');
+var useref = require('gulp-useref');
+var gulpif = require('gulp-if');
+var uglify = require('gulp-uglify');
 
 gulp.task('minify-css', function() {
     return gulp.src('css/*.css')
       .pipe(concatCSS('css/bundle.css'))
       .pipe(cleanCSS())
       .pipe(gulp.dest(''));
+});
+
+gulp.task('useref', function() {
+	return gulp.src('index.html')
+	  .pipe(useref())
+	  .pipe(gulpif('*.css', cleanCSS()))
+	  .pipe(gulpif('*.js', uglify()))
+	  .pipe(gulp.dest('dist'));
 });
