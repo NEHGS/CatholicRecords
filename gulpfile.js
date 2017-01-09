4ffcc98a8ca7d447ca98fa53460774301a343a72
Andy Hanson-Dvoracek <andrew.hanson.dvoracek@nehgs.org>
2017-01-09 15:50:23 -0500

    Build concat CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
 var cleanCSS = require('gulp-clean-css');
+var concatCSS = require('gulp-concat-css');
 
 gulp.task('minify-css', function() {
     return gulp.src('css/*.css')
+      .pipe(concatCSS('css/bundle.css'))
       .pipe(cleanCSS())
-      .pipe(gulp.dest('dist'));
+      .pipe(gulp.dest(''));
 });
